5ca47f846c6ec1a3048daeda518f3c27d7bb9526
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-05-06 22:46:46 +0530

    Triangle pattern

--- Triangle.c
+++ Triangle.c
@@ -2,7 +2,7 @@
 int main()
 {
     int row,col,number;
-    printf("Enter a number\n");
+    printf("Enter a number of rows and columns \n");
     scanf("%d",&number);
     for(row = 1;row<=number;row++,printf("\n")){
         for(col=row;col<number;col++){
